8afa11af9cde2540b626c354ca6bf56db3fdbbb5
Carlo Alberto Degli Atti <lordkada@Carlos-MacBook-Pro.local>
2015-07-27 07:48:34 +0200

    refactoring and fixing bower to require jquery

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 # updates the JS from the master branch by building the demo package
 git clone -b 3.0.5 --single-branch https://github.com/cometd/cometd.git
-cd cometd
-mvn package -pl cometd-demo -am -Dmaven.test.skip=true && \
-cp -Rv cometd-javascript/common/target/cometd-javascript-common-*/org . && \
-cd ..
+
+cat ./cometd/cometd-javascript/jquery/src/main/webapp/jquery/jquery.cometd.js \
+     ./cometd/cometd-javascript/jquery/src/main/webapp/jquery/jquery.cometd-ack.js \
+     ./cometd/cometd-javascript/jquery/src/main/webapp/jquery/jquery.cometd-reload.js \
+     ./cometd/cometd-javascript/jquery/src/main/webapp/jquery/jquery.cometd-timestamp.js \
+     ./cometd/cometd-javascript/jquery/src/main/webapp/jquery/jquery.cometd-timesync.js > ./dist/cometd.js
+
 rm -rf cometd
-git status
