6a8cc1f0ce7e39b7b4eb92800e7170cfbfae14b0
Carlo Alberto Degli Atti <lordkada@Carlos-MacBook-Pro.local>
2015-07-27 07:14:07 +0200

    cometd library v3.0.5

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # updates the JS from the master branch by building the demo package
-git clone https://github.com/cometd/cometd.git
+git clone -b 3.0.5 --single-branch https://github.com/cometd/cometd.git
 cd cometd
 mvn package -pl cometd-demo -am -Dmaven.test.skip=true && \
 cp -Rv cometd-javascript/common/target/cometd-javascript-common-*/org . && \
